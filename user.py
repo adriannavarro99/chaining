c015fe5cbaf513ed26b7c2af625e91eb6a8e6cc3
adriannavarro99 <adriannavarro92@gmail.com>
2021-09-21 15:24:10 -0600

    fist commit

---
+++ user.py
@@ -0,0 +1,30 @@
+class bank:
+
+
+    def __init__ (self, accountNum, owner,balance):
+        self.accountNum =accountNum
+        self.owner=owner
+        self.balance=balance
+        
+    
+
+    def withdrawal (self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+        else:
+            print("We cannot process your withdrawal.")
+            print(f"You currently have {self.balance}.")
+            print(f"And you are trying to withdraw{amount}.")
+        return self  
+
+
+    def display_user_balance(self):
+        print(f"Account balance:{self.owner} : {self.balance}.")
+        return self
+    
+
+
+
+    def deposit (self, amount):
+        self.balance+= amount
+        return self
